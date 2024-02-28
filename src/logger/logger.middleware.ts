323b21f00c535faf4bd349ef7e3b27c692d1f256
Pranit Khadilkar <pranit@emtech.com>
2024-02-28 14:44:49 +0530

    added ip in the logger middleware

--- src/logger/logger.middleware.ts
+++ src/logger/logger.middleware.ts
@@ -5,13 +5,13 @@ import { NextFunction, Request, Response } from 'express'
 export class LoggerMiddleware implements NestMiddleware {
   private readonly logger = new Logger('HTTP')
   use(request: Request, response: Response, next: NextFunction) {
-    const { method, originalUrl } = request
+    const { ip, method, originalUrl } = request
     const userAgent = request.get('user-agent') || ''
-    this.logger.log(`Starting ${method} ${originalUrl} - ${userAgent}`)
+    this.logger.log(`Starting ${method} ${originalUrl} - ${userAgent} ${ip}`)
     response.on('finish', () => {
       const { statusCode } = response
       this.logger.log(
-        `Ending ${method} ${originalUrl} ${statusCode} - ${userAgent}`,
+        `Ending ${method} ${originalUrl} ${statusCode} - ${userAgent} ${ip}`,
       )
     })
     next()
